6e8fbe25aea7a4a225344d20e2b55b3a6c297d87
Fabrício Liberato <fabricio.liber@gmail.com>
2017-11-13 20:02:10 -0300

    Definindo struct e assinatura das funções

--- fila.h
+++ fila.h
@@ -1,1 +1,18 @@
-//TODO
+typedef int telem;
+
+typedef struct no {
+  telem espera;
+  telem atendimento;
+  struct no prox;
+} tno;
+
+typdef struct tfila {
+  tno* inicio;
+  tno* fim;
+}
+
+void criarFila (tfila *F);
+int vaziaFila (tfila F);
+int primeiroFila (tfila F);
+int inserirFila (tfila *F, telem atendimento, telem espera);
+int removerFila (tfila *F, telem atendimento, telem espera);
