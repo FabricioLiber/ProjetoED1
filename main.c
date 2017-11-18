9f27c2d5d77605ab4e957735c07a052f83e96ac5
Rafael Lins <rlinsdesouza@gmail.com>
2017-11-17 21:21:27 -0300

    modularizando

--- main.c
+++ main.c
@@ -17,44 +17,27 @@ void main () {
   printf("\e[H\e[2J");
   criarFila (&F);
   srand(time(NULL));
-  //inserirFila(&F, 1, 3, 2);
-  //inserirFila(&F, 2, 1, 3);
-  //inserirFila(&F, 3, 2, 4);
-  //inserirFila(&F, 4, 5, 3);
-  //removerFila (&F, &idCli);
-  //primeiroFila(F,&idCli);
-  //printf ("%d\n",idCli);
-  //printf("%d\n",tamanhoFila(F));
-  //imprimirFila(F);
 
   for (i = 0; i < TEMPO; i++,minutos++) {
     printf("\e[H\e[2J");
     printf("TEMPO DO PROGRAMA: %d MINUTO%s\n",minutos, minutos == 1 ? "" : "S");
+
     ///cria o Cliente
     criaCliente (&clienteEntrada,minutos,&idCli);
-    /*if (minutos == 0 || clientes[0].entrada < minutos) {
-			clientes[0].id = ++idCli;
-			clientes[0].entrada = minutos + (1+(rand() % 4));
-      clientes[0].atendimento = (1+(rand() % 4));
-    }*/
     printf("CLIENTE: PARA CHEGAR NA FILA:%d(%d,%d)\n", clienteEntrada.id, clienteEntrada.entrada, clienteEntrada.atendimento);
-    ///chegada na fila
-    if (clienteEntrada.entrada == minutos ) {
-      inserirFila(&F, clienteEntrada.id, clienteEntrada.entrada, clienteEntrada.atendimento);
-    }
+
+    ///entrada na fila
+		entraFila (&F,&clienteEntrada,minutos);
+
    //atendimento no caixa
 		if (flagCaixa==0 ) {
 			if (!vaziaFila(F)) {
-				removerFila(&F,&idCli,&tAtend);
-      	clienteAtendimento.id = clienteEntrada.id;
-      	clienteAtendimento.atendimento = clienteEntrada.atendimento;
+				entraCaixa (&F,&clienteAtendimento);
 				printf ("CAIXA EM ATENDIMENTO: Cliente %d (tempo previsto atendimento %d)\n", clienteAtendimento.id, clienteAtendimento.atendimento);
       	flagCaixa = 1;
 			}else {
 				printf("CAIXA LIVRE!\n");
 			}
-
-
     //sair do atendimento e caixa vazio
     } else {
 			printf ("CAIXA EM ATENDIMENTO: Cliente %d (tempo previsto atendimento %d)\n", clienteAtendimento.id, clienteAtendimento.atendimento);
@@ -70,7 +53,7 @@ void main () {
     //sleep(2);
 		//Pausa manual do programa;
 		fflush(stdin);
-		printf("Pressione ENTER \n");
+		printf("\nPressione ENTER \n");
 		getchar();
 
   }
